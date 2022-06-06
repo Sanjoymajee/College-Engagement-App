fc1800818be25a2698d4de86183f6e8f77fc65e2
Sanjoymajee <sanjoymajee02@gmail.com>
2022-06-07 02:12:48 +0530

    improve Like Button

--- public/js/voting.js
+++ public/js/voting.js
@@ -1,21 +1,23 @@
 const upVoted = () => {
     const blogId = document.getElementById('blogId').innerHTML;
     const msg = document.getElementById('message').innerHTML;
+    const votes = document.getElementById('updatedVote').innerHTML;
     if(msg == 'notLiked'){
         document.getElementById('liked').style.display = 'block';
         document.getElementById('not-liked').style.display = 'none';
         document.getElementById('message').innerHTML = 'liked';
+        document.getElementById('updatedVote').innerHTML = Number(votes)+1;
     }
     else if(msg == 'liked'){
         document.getElementById('not-liked').style.display = 'block';
         document.getElementById('liked').style.display = 'none';
         document.getElementById('message').innerHTML = 'notLiked';
+        document.getElementById('updatedVote').innerHTML = Number(votes)-1;
     }
     const req = new XMLHttpRequest;
     req.onreadystatechange = async function () {
         if (this.readyState == 4 && this.status == 200) {
             let data = await JSON.parse(this.responseText);
-            document.getElementById('updatedVote').innerHTML = data.upvote;
         }
     };
     req.open('GET', `https://college-blog-app.herokuapp.com/blog/vote/${blogId}/1`, true);
